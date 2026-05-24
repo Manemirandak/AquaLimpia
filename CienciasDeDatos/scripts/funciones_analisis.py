18a537a877435707c9ee808779d9c9809900fbca
Manemirandak <143052973+Manemirandak@users.noreply.github.com>
2026-05-24 15:31:12 -0400

    ArchivosDesdeGoogleColab

---
+++ CienciasDeDatos/scripts/funciones_analisis.py
@@ -0,0 +1,39 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# FUNCION PARA CARGAR DATOS
+def cargar_datos(ruta):
+
+    df = pd.read_excel(ruta)
+
+    return df
+
+
+# FUNCION PARA CALCULAR PROMEDIOS
+def calcular_promedios(df):
+
+    resumen = df.groupby("planta")[[
+        "DBO_entrada_mg_L",
+        "DBO_salida_mg_L",
+        "energia_aeracion_kWh",
+        "lodos_generados_kg_d"
+    ]].mean()
+
+    return resumen
+
+
+# FUNCION PARA GRAFICO
+def grafico_dbo(resumen):
+
+    resumen["DBO_salida_mg_L"].plot(
+        kind="bar",
+        figsize=(8,5)
+    )
+
+    plt.title("DBO salida promedio")
+    plt.ylabel("DBO mg/L")
+    plt.xticks(rotation=45)
+
+    plt.show()
